1265b2ea0a9de4e9276b73da91f2de87d9f2cba1
keyanap <keyanapregent@keyanas-mbp.home>
2023-09-07 19:21:27 -0400

    this is my third commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,22 +1,57 @@
 import os
 import csv
 
-budget_data_csv = os.path.join("..", "Resources", "budget_data.csv")
+budget_data_csv = os.path.join("Resources", "budget_data.csv")
 
 date = []
-profit = []
+total_months = 0
+total_net = 0
+net_change_list = []
+greatest_increase = ["",0]
+greatest_decrease = ["",99999999999]
+avg_change = 0
 
 with open(budget_data_csv) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter= " ,")
+    csvreader = csv.reader(csvfile, delimiter= ",")
+    header=next(csvreader)
+    first_row = next(csvreader)
+    total_months += 1
+    total_net += int(first_row[1])
+    previous_net = int(first_row[1])
 
-    date.append = (row[0])
-    profit.append = (row[1])
+    for row in csvreader:
+        total_months += 1
+        total_net += int(row[1])
+        net_change = int(row[1])-previous_net
+        previous_net = int(row[1])
+        net_change_list += [net_change]
+        date.append (row[0])
+        
+        if net_change > greatest_increase[1]:
+            greatest_increase[0] = row[0]
+            greatest_increase[1] = net_change
+        if net_change < greatest_decrease[1]:
+            greatest_decrease[0] = row[0]
+            greatest_decrease[1] = net_change
+    avg_change = sum(net_change_list)/len(net_change_list)
 
 print("Financial Analysis")
-print("Total Months: " + date.count)
-print["Total: " + sum(row[1])]
-print["Average Change: " + sum(row[1])/len(row[1])]
-print("Greatest Increase in Profits: " + max(row[1]))
-print("Greatest Decrease in Profits: " + max(row[1]))
+print("-------------------------")
+print("Total Months: ", total_months)
+print("Total: ", total_net)
+print("Average Change:" + "$", (round(avg_change,2)))
+print("Greatest Increase in Profits:" + greatest_increase[0] + "(" + "$", greatest_increase[1], ")")
+print("Greatest Decrease in Profits:" + greatest_decrease[0]+ "(" + "$", greatest_decrease[1], ")")
 
-# export results to a text file
+profitanalysis = os.path.join("Analysis", "Profit Analysis.txt")
+with open (profitanalysis,"w") as profitdata:
+    
+    output = (f"Profit Analysis\n"
+    f"----------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total: {total_net}\n"
+    f"Average Change: ${round(avg_change,2)}\n"
+    f"Greatest Increase in Profits: {greatest_increase[0]} ${greatest_increase[1]}\n"
+    f"Greatest Decrease in Profits: {greatest_decrease[0]} ${greatest_decrease[1]}")
+
+    profitdata.write(output)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,20 +8,31 @@ county =[]
 candidate =[]
 
 with open(election_data_csv) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter= " ,")
+    csvreader = csv.reader(csvfile, delimiter= ",")
+    header=next(csvreader)
+    for row in csvreader:
+        candidate.append (row[2])
 
-    ballot_id.append = (row[0])
-    county.append = (row[1])
-    candidate.append = (row[2])
-
-    # ignore header
     import statistics
-   
+charlespercentage =candidate.count("Charles Casper Stockham")/len(candidate)*100
+dianapercentage = candidate.count("Diana DeGette")/len(candidate)*100
+raymonpercentage = candidate.count("Raymon Anthony Doane")/len(candidate)*100
+
 print("Election Results")
-print("Total Votes: " + ballot_id.count)
-print("Charles Casper Stockham:" + "(" + candidate.count(Charles Casper Stockham) + ")")
-print("Diana DeGette: " + "(" + candidate.count(Diana DeGette)+ ")")
-print("Raymon Anthony Doane: " + "(" + candidate.count(Raymon Anthony Doane) + ")")
-print("Winner:" + statistics.mode(row[2]))
-# percentage for votes 
+print("Total Votes: ", len(candidate))
+print("Charles Casper Stockham:", charlespercentage,"%" + "(", candidate.count("Charles Casper Stockham"), ")")
+print("Diana DeGette: ", dianapercentage,"%" + "(", candidate.count("Diana DeGette"), ")")
+print("Raymon Anthony Doane: ", raymonpercentage,"%" + "(", candidate.count("Raymon Anthony Doane"), ")")
+print("Winner:" + statistics.mode(candidate))
+
+electionresults = os.path.join("Analysis", "Election Results.txt")
+with open (electionresults,"w") as electiondata:
     
+    output = (f"Election Results\n"
+    f"Total Votes: {len(candidate)}\n"
+    f"Charles Casper Stockham: {charlespercentage}% '('{candidate.count('Charles Casper Stockham')}')'\n"
+    f"Diana DeGette: {dianapercentage}% '(' {candidate.count('Diana DeGette')}')'\n"
+    f"Raymon Anthony Doane: {raymonpercentage} % '('{candidate.count('Raymon Anthony Doane')}')'\n"
+    f"Winner: {statistics.mode(candidate)}")
+
+    electiondata.write(output)
